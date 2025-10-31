a711b6422897f880e703a7d022303bba9c90036f
MiyalalMulla <mullamiyalal@gmail.com>
2025-10-31 10:06:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateTimer() {
+      const now = new Date(); // Get current system time
+      const date = now.toLocaleDateString(); // Format date
+      const time = now.toLocaleTimeString(); // Format time
+      document.getElementById("timer").textContent = `${date} ${time}`;
+    }
+    updateTimer();
+    setInterval(updateTimer, 1000);
